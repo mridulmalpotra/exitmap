b2a237beb820eba4332e6a6190af00928a451a86
Mridul Malpotra <mridul.malpotra@gmail.com>
2016-07-22 17:59:17 +0530

    Added consensusupdate.py
    Separate file for asychronously updating the
    cached consensus.

---
+++ src/consensusupdate.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+
+# TODO: Will daemonizing the exitmap process lead to the asynch call to be
+# daemonized as well? Check and test the same.
+
+
+import sys
+import stem
+import multiprocessing
+from time import sleep
+
+
+def updateConsensus():
+    """
+    Asynchronous process for updating the process
+    """
+    pass
+
+
+def run():
+    """
+    Main function to create process Pool and call
+    """
+    pass
