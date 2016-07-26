f53950254ce7252ea4e578bb72d3df6835f73ddc
Mridul Malpotra <mridul.malpotra@gmail.com>
2016-07-26 10:40:32 +0530

    continuous-scanning: added consensus updating

--- src/consensusupdate.py
+++ src/consensusupdate.py
@@ -5,7 +5,7 @@
 
 
 import sys
-import stem
+from stem.descriptor.remote import DescriptorDownloader
 import multiprocessing
 from time import sleep
 
@@ -14,11 +14,32 @@ def updateConsensus():
     """
     Asynchronous process for updating the process
     """
-    pass
+    consensus_downloader = DescriptorDownloader(
+    use_mirrors = True,
+    retries = 10,
+    timeout = 30,
+    )
 
+    query = consensus_downloader.get_server_descriptors()
+    try:
+        for desc in query.run():
+            if desc.exit_policy.is_exiting_allowed():
+                print '  %s (%s)' % (desc.nickname, desc.fingerprint)
+    except Exception as exc:
+      print 'Unable to retrieve the server descriptors: %s' % exc
 
 def run():
     """
     Main function to create process Pool and call
     """
-    pass
+    pool = multiprocessing.Pool()
+    while (True):
+        pool.apply_async(updateConsensus, args = ())
+    pool.close()
+    pool.join()
+
+
+def main():
+    """
+    Default settings when run from the command line
+    """
